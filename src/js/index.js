423e6fad15afce913b2fe8237248c32d93587713
dzony6969 <dzony6969@users.noreply.github.com>
2020-05-19 23:52:25 +0200

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,88 +1,41 @@
-import SERVICE from "./API/API.js";
-import { sumAll, avgIncome } from "./components/math.js";
-import { pagination } from "./pagination/pagination.js";
-import { search } from "./components/search.js";
-import { sortTable } from "./components/sort.js";
-
 const DOM = {
-  table: document.querySelector("#dataTable"),
-  btn: document.querySelector("#search"),
-  btnClean: document.querySelector("#clean"),
-  th: document.getElementsByTagName("th"),
-  lastMonth: document.querySelector("#lastMonth"),
-};
-const d = new Date();
-d.setMonth(d.getMonth() - 1);
-const prevMonth = d.toJSON().slice(0, 7);
-
-const getData = async () => {
-  const response = await SERVICE.fetchData();
-  response.forEach(async (company, index) => {
-    const response = await SERVICE.fetchIncome(company.id);
-    company.income = response.incomes;
-    company.income;
-
-    let filtered = company.income.filter((val) => {
-      return val.date.slice(0, 7) == prevMonth;
-    });
-    function sumPrevMonth(arr) {
-      return filtered.reduce((prev, cur) => {
-        return prev + parseFloat(cur.value);
-      }, 0);
-    }
-    const prevMonthIncome = sumPrevMonth();
-    let total = sumAll(company.income);
-    let avg = avgIncome(company.income);
-    const loader = `
-    <tr class='item item-${index}' id=${company.id}>
-    <td>${company.id}</td>
-    <td>${company.name}</td>
-    <td>${company.city}</td>
-    <td>${total.toFixed(2)} $</td>
-    <td>${avg} $</td>
-    <td>${prevMonthIncome.toFixed(2)} $</td>
-  </tr>
-    `;
-    DOM.table.insertAdjacentHTML("beforeend", loader);
-  });
-  document.querySelector(".dataSet").style.display = "block";
-  document.querySelector(".loading").style.display = "none";
+  form: document.querySelector("#send-palidrom"),
+  name: document.querySelector("#user-name"),
+  email: document.querySelector("#user-email"),
+  palidrom: document.querySelector("#user-palidrom"),
+  cta: document.querySelector("#send"),
+  answer: document.querySelector(".answer"),
 };
 
-const cleanInput = () => {
-  DOM.btnClean.addEventListener("click", () => {
-    let input = (document.getElementById("search").value = "");
-    document.querySelectorAll("a").forEach((e) => {
-      e.classList.remove("active");
-    });
-    search();
-  });
+const emailIsValid = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
-const searchInit = () => {
-  DOM.btn.addEventListener("keyup", () => {
-    if (event.keyCode === 13) {
-      search();
-    }
-  });
+const controller = (event) => {
+  event.preventDefault();
+  if (DOM.name.value == "") {
+    DOM.name.style.border = "1px solid red";
+  } else {
+    DOM.name.style.border = "none";
+  }
+  if (DOM.palidrom.value == "") {
+    DOM.palidrom.style.border = "1px solid red";
+  } else {
+    DOM.palidrom.style.border = "none";
+  }
+  if (!emailIsValid(DOM.email.value)) {
+    DOM.email.style.border = "1px solid red";
+  } else {
+    DOM.email.style.border = "none";
+  }
+  if (
+    DOM.name.value != "" &&
+    DOM.palidrom.value != "" &&
+    emailIsValid(DOM.email.value)
+  ) {
+    DOM.form.style.display = "none";
+    DOM.answer.style.display = "block";
+  }
 };
 
-for (let c = 0; c < DOM.th.length; c++) {
-  DOM.th[c].addEventListener("click", item(c));
-}
-
-function item(c) {
-  return function () {
-    sortTable(c);
-  };
-}
-
-const init = () => {
-  getData();
-  pagination();
-  searchInit();
-  cleanInput();
-  DOM.lastMonth.innerHTML = `Last month income (${prevMonth})`;
-};
-
-init();
+DOM.cta.addEventListener("click", controller);
