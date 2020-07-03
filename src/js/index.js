54ff0930420b5e33d8180d410913b26745ce7096
dzony6969 <dzony6969@users.noreply.github.com>
2020-07-03 21:05:34 +0200

    solution code

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,87 @@
-const DOM = {
-  form: document.querySelector("#send-palidrom"),
-  name: document.querySelector("#user-name"),
-  email: document.querySelector("#user-email"),
-  palidrom: document.querySelector("#user-palidrom"),
-  cta: document.querySelector("#send"),
-  answer: document.querySelector(".answer"),
+const sliderItems = document.querySelector(".slider-items").children;
+const lightBoxImage = document.querySelector(".lightbox-img");
+const counter = document.querySelector(".lightbox-counter");
+const lightBoxClose = document.querySelector(".lightbox-close");
+const lightBoxText = document.querySelector(".lightbox-text");
+const lightBoxContainer = document.querySelector(".lightbox");
+const nextWrapper = document.querySelector(".next");
+const prevWrapper = document.querySelector(".prev");
+const prevChild = document.querySelector(".prev span");
+const nextChild = document.querySelector(".next span");
+
+console.log(prevChild);
+let index;
+let imgSrc;
+
+for (let i = 0; i < sliderItems.length; i++) {
+  sliderItems[i].querySelector(".fa").addEventListener("click", () => {
+    // console.log(sliderItems[i].querySelector("h2"));
+    index = i;
+    changeImage();
+    lightBox();
+  });
+}
+
+const lightBox = () => {
+  lightBoxContainer.classList.toggle("open");
 };
 
-const emailIsValid = (email) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+const changeImage = () => {
+  imgSrc = sliderItems[index].querySelector("img").getAttribute("src");
+  lightBoxImage.src = imgSrc;
+  counter.innerHTML = `${index + 1} z ${sliderItems.length}`;
+
+  lightBoxText.innerHTML = sliderItems[index].querySelector("h2").innerHTML;
+};
+const nextImage = () => {
+  if (index == sliderItems.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeImage();
 };
 
-const controller = (event) => {
-  event.preventDefault();
-  if (DOM.name.value == "") {
-    DOM.name.style.border = "1px solid red";
+const prevImage = () => {
+  if (index == 0) {
+    index = sliderItems.length - 1;
   } else {
-    DOM.name.style.border = "none";
+    index--;
   }
-  if (DOM.palidrom.value == "") {
-    DOM.palidrom.style.border = "1px solid red";
-  } else {
-    DOM.palidrom.style.border = "none";
-  }
-  if (!emailIsValid(DOM.email.value)) {
-    DOM.email.style.border = "1px solid red";
-  } else {
-    DOM.email.style.border = "none";
-  }
+  changeImage();
+};
+lightBoxContainer.addEventListener("click", () => {
   if (
-    DOM.name.value != "" &&
-    DOM.palidrom.value != "" &&
-    emailIsValid(DOM.email.value)
+    event.target !== lightBoxImage &&
+    event.target !== nextWrapper &&
+    event.target !== prevWrapper &&
+    event.target !== prevChild &&
+    event.target !== nextChild
   ) {
-    DOM.form.style.display = "none";
-    DOM.answer.style.display = "block";
+    lightBox();
+    console.log("closed");
+  }
+});
+
+nextWrapper.addEventListener("click", nextImage);
+prevWrapper.addEventListener("click", prevImage);
+
+document.onkeydown = (e) => {
+  if (lightBoxContainer.classList.contains("open")) {
+    switch (e.keyCode) {
+      case 37:
+        prevImage();
+        break;
+
+      case 39:
+        nextImage();
+        break;
+    }
   }
 };
+const zoomOnClick = () => {
+  lightBoxImage.classList.toggle("scale-up");
+  lightBoxImage.classList.toggle("scale-down");
+};
 
-DOM.cta.addEventListener("click", controller);
+lightBoxImage.addEventListener("click", zoomOnClick);
